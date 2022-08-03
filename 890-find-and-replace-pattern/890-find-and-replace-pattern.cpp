f740cc380fc0f3fc7e707f15aa3d5ceccc94cf2e
Adarsh-Raj-Jaiswal <91599466+Adarsh-Raj-Jaiswal@users.noreply.github.com>
2022-08-03 08:04:56 +0530

    Time: 11 ms (21.94%), Space: 8.8 MB (8.45%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -1,14 +1,26 @@
 class Solution {
 public:
-    vector<string> findAndReplacePattern(vector<string> words, string p) {
-        vector<string> res;
-        for (string w : words) if (F(w) == F(p)) res.push_back(w);
-        return res;
+    vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+        vector<string>result;
+        for(int i=0;i<words.size();i++){
+            
+            if(findPattern(words[i])==findPattern(pattern)){
+                
+                result.push_back(words[i]);
+            }
+        }
+        return result;
     }
-    string F(string w) {
-        unordered_map<char, int> m;
-        for (char c : w) if (!m.count(c)) m[c] = m.size();
-        for (int i = 0; i < w.length(); ++i) w[i] = 'a' + m[w[i]];
-        return w;
+    string findPattern(string s){
+        unordered_map<char,int> map;
+        for(int i=0;i<s.length();i++){
+            if(!map.count(s[i])){
+                map[s[i]] = map.size();
+            }
+        }
+        for(int i=0;i<s.length();i++){
+            s[i] = 'a' + map[s[i]];
+        }
+        return s;
     }
 };
