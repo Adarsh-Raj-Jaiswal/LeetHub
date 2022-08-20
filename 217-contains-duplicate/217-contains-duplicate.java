f29bba4d427394b6e0d80310cc4430120f58f29f
Adarsh-Raj-Jaiswal <91599466+Adarsh-Raj-Jaiswal@users.noreply.github.com>
2022-08-20 15:55:12 +0530

    Time: 13 ms (59.31%), Space: 69.5 MB (52.77%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -0,0 +1,16 @@
+class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+        
+//     }
+    public boolean containsDuplicate(int[] nums) {
+
+    final Set<Integer> distinct = new HashSet<Integer>();
+    for(int num : nums) {
+        if(distinct.contains(num)) {
+            return true;
+        }
+        distinct.add(num);
+    }
+    return false;
+}
+}
