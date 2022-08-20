4e20558ee4f731e170f82e09cb324d618060eaa5
Adarsh-Raj-Jaiswal <91599466+Adarsh-Raj-Jaiswal@users.noreply.github.com>
2022-08-20 15:50:35 +0530

    Time: 199 ms (15.54%), Space: 52.2 MB (35.00%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int>val;
+        for(int i=0;i<nums.size();i++){
+            if(val.count(nums[i]))return true;
+            val.insert(nums[i]);
+        }
+        return false;
+    }
+};
