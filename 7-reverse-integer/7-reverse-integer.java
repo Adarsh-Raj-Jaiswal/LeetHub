7c187d6bb4da43757e49acecd832cd198d925903
Adarsh-Raj-Jaiswal <91599466+Adarsh-Raj-Jaiswal@users.noreply.github.com>
2022-08-22 14:49:56 +0530

    Time: 2 ms (78.43%), Space: 41.2 MB (65.47%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.java
+++ 7-reverse-integer/7-reverse-integer.java
@@ -1,11 +1,8 @@
 class Solution{
     public int reverse(int x){
-        int reverse=0;
-        while(x!=0){
+        int reverse;
+        for(reverse=0;x!=0;reverse=(reverse*10)+(x%10),x=x/10)
             if(reverse>2147483647/10||reverse<-2147483648/10)return 0;
-            reverse=(reverse*10)+(x%10);
-            x=x/10;
-        }
         return reverse;
     }
 }
